45b39d2675c0a24b89815a783df92f2e671bba02
sumin <rntnalseee@gmail.com>
2022-05-26 10:54:37 +0900

    1015

---
+++ problem_100/src/problem_100/run/Run.java
@@ -0,0 +1,12 @@
+package problem_100.run;
+
+import problem_100.solve.Solve;
+
+public class Run {
+	public static void main(String[] args) {
+		Solve s = new Solve();
+		s.problem_1015();
+		
+	}
+
+}
---
+++ problem_100/src/problem_100/solve/Solve.java
@@ -0,0 +1,11 @@
+package problem_100.solve;
+
+import java.util.Scanner;
+
+public class Solve {
+	public void problem_1015() {
+		Scanner sc = new Scanner(System.in);
+		// float f1 = sc.nextFloat();
+		System.out.printf("%.2f", sc.nextFloat());
+	}
+}
