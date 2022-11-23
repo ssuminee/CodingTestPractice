c5ed93a93b4a6bf912cd520edc0dcdfa9d2e6153
sumin <rntnalseee@gmail.com>
2022-11-23 21:16:22 +0900

    [Silver IV] Title: 한수, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(arithmetic(sc.nextInt()));
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(arithmetic(Integer.parseInt(br.readLine())));
     }
     
     public static int arithmetic(int num){
